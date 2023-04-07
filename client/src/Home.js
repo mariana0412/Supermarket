672def88d3fe3281e462eafada37f160e592667c
Mariana Piz <marianapiz0412@gmail.com>
2023-04-07 23:53:11 +0300

    Redesign Home page

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,19 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import AppNavbar from './AppNavbar';
 import { Link } from 'react-router-dom';
-import { Button, Container } from 'reactstrap';
+import { Button, Container, Form, FormGroup, Input, Label } from 'reactstrap';
 
 const Home = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleLoginSubmit = (event) => {
+        event.preventDefault();
+        console.log('Logging in with username:', username, 'and password:', password);
+    };
+
     return (
         <div>
-            <AppNavbar/>
+            <AppNavbar />
+            <header className="header">
+                <h1>Supermarket Zlagoda</h1>
+                <Form onSubmit={handleLoginSubmit} className="login-form">
+                    <FormGroup>
+                        <Label for="username">Username</Label>
+                        <Input type="text" name="username" id="username" value={username} onChange={handleUsernameChange} />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="password">Password</Label>
+                        <Input type="password" name="password" id="password" value={password} onChange={handlePasswordChange} />
+                    </FormGroup>
+                    <Button color="primary" type="submit">
+                        Login
+                    </Button>
+                </Form>
+            </header>
+
             <Container fluid>
-                <Button color="link"><Link to="/categories">Manage Categories</Link></Button>
-                <Button color="link"><Link to="/employees">Manage Employees</Link></Button>
+                <Button color="link">
+                    <Link to="/categories">Manage Categories</Link>
+                </Button>
+                <Button color="link">
+                    <Link to="/employees">Manage Employees</Link>
+                </Button>
             </Container>
         </div>
     );
-}
+};
 
 export default Home;
