408ff46b489d0479832f3ac460d1ae40786616ed
Mariana Piz <marianapiz0412@gmail.com>
2023-04-11 17:11:58 +0300

    Implement searching for contact info feature

--- client/src/Employee/EmployeeList.js
+++ client/src/Employee/EmployeeList.js
@@ -1,13 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import { Button, ButtonGroup, Container, Table, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import {Button, ButtonGroup, Container, Table, Dropdown, DropdownToggle, DropdownMenu, DropdownItem,
+    Modal, ModalHeader, ModalBody, ModalFooter, Input
+} from 'reactstrap';
 import AppNavbar from '../AppNavbar';
 import { Link } from 'react-router-dom';
+import '../App.css';
 
 const EmployeeList = () => {
 
     const [employees, setEmployees] = useState([]);
+    const [contactInfo, setContactInfo] = useState(null);
+    const [searchSurname, setSearchSurname] = useState('');
     const [dropdownOpen, setDropdownOpen] = useState(false);
+    const [modal, setModal] = useState(false);
     const [sortOption, setSortOption] = useState(null);
+
 
     useEffect(() => {
         let url = 'api/employees';
@@ -53,9 +60,35 @@ const EmployeeList = () => {
             setSortOption(option);
     }
 
+    const handleSearchInputChange = (event) => {
+        setSearchSurname(event.target.value);
+    }
+
     const toggleDropdown = () => {
         setDropdownOpen(!dropdownOpen);
     }
+
+    const toggleModal = () => {
+        setModal(!modal);
+        if(modal)
+            setSearchSurname('');
+    };
+
+    const showContactInfo = async () => {
+        try {
+            const response = await fetch(`/api/employees/contact-info?surname=${searchSurname}`);
+            if (response.status === 204) {
+                alert('There is no employee with such a surname.');
+            } else {
+                const data = await response.json();
+                setContactInfo(data[0]);
+                toggleModal();
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
 
     const employeeList = employees.map(employee => {
         return <tr key={employee.id_employee}>
@@ -95,6 +128,10 @@ const EmployeeList = () => {
                 <Button className="float-end" onClick={() => window.print()}>Print</Button>
                 <Button className="float-end" style={{ marginRight: '20px' }} color="success" tag={Link}
                         to="/employees/new">Add Employee</Button>
+                <div className='search-container'>
+                    <Input style={{width: '200px' }} type="text" placeholder="Search by Surname" value={searchSurname} onChange={handleSearchInputChange} />
+                    <Button color="primary" onClick={() => showContactInfo()}>Search</Button>
+                </div>
 
 
                 <Table className="mt-4">
@@ -118,6 +155,25 @@ const EmployeeList = () => {
                     {employeeList}
                     </tbody>
                 </Table>
+
+                <Modal isOpen={modal} toggle={toggleModal}>
+                    <ModalHeader toggle={toggleModal}>Contact Info</ModalHeader>
+                    <ModalBody>
+                        {contactInfo && (
+                            <>
+                                <p>Phone Number: {contactInfo?.phone_number}</p>
+                                <p>City: {contactInfo?.city}, {contactInfo?.street}, {contactInfo?.zip_code}</p>
+                                <p>Street: {contactInfo?.street}</p>
+                                <p>Zip-code: {contactInfo?.zip_code}</p>
+                            </>
+                        )}
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="secondary" onClick={toggleModal}>Close</Button>
+                    </ModalFooter>
+                </Modal>
+
+
             </Container>
         </div>
     );
