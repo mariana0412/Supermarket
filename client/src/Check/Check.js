fc14c59f6d947a088abbe5c1dd68b0bcaa69ae45
Mariana Piz <marianapiz0412@gmail.com>
2023-04-13 16:52:00 +0300

    Separate Check from CheckList

---
+++ client/src/Check/Check.js
@@ -0,0 +1,23 @@
+import {Button, ButtonGroup} from "reactstrap";
+import React from "react";
+
+const Check = ({ check, remove, showPurchasedProducts }) => {
+    return (
+        <tr key={check.check_number}>
+        <td>{check.check_number}</td>
+        <td>{check.id_employee}</td>
+        <td>{check.card_number}</td>
+        <td>{check.print_date}</td>
+        <td>{check.sum_total}</td>
+        <td>{check.vat}</td>
+        <td>
+            <ButtonGroup>
+                <Button size="sm" color="danger" onClick={() => remove(check.check_number)}>Delete</Button>
+                <Button size="sm" color="primary" onClick={() => showPurchasedProducts(check.check_number)}>View Details</Button>
+            </ButtonGroup>
+        </td>
+    </tr>
+    );
+};
+
+export default Check;
