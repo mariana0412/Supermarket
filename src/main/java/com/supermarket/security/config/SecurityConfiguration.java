257ed2177ddbbb68322dffe1cc7d157d9f1f71a8
Mariana Piz <marianapiz0412@gmail.com>
2023-04-24 16:05:44 +0300

    Allow access to endpoint only authenticated users

--- src/main/java/com/supermarket/security/config/SecurityConfiguration.java
+++ src/main/java/com/supermarket/security/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.supermarket.security.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,26 +25,38 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .anyRequest().permitAll() // TODO: finish permits on frontend
-                /*.requestMatchers("/api/auth/**").permitAll()
-
+                .requestMatchers("/api/auth/**").permitAll()
+                .anyRequest().authenticated()
+/**
+                // add category, product, store product and employee can only manager
                 .requestMatchers(HttpMethod.POST,
                         "/api/categories",
                         "/api/products",
                         "/api/store-products",
-                        "/api/employees",
-                        "/api/customer-cards")
-                .hasRole("MANAGER")
+                        "/api/employees")
+                .hasRole("ROLE_MANAGER")
 
-                .requestMatchers(HttpMethod.POST, "/api/checks").hasRole("CASHIER")
+                // add check and sale can only cashier
+                .requestMatchers(HttpMethod.POST,
+                        "/api/checks",
+                        "/api/sales")
+                .hasRole("ROLE_CASHIER")
 
+                // add customer card can both manager and cashier
+                .requestMatchers(HttpMethod.POST, "/api/customer-cards").authenticated()
+
+                // edit category, product, store product and employee can only manager
                 .requestMatchers(HttpMethod.PUT,
                         "/api/categories/**",
                         "/api/products/**",
                         "/api/store-products/**",
                         "/api/employees/**")
-                .hasRole("MANAGER")
+                .hasRole("ROLE_MANAGER")
 
+                // edit customer card can both manager and cashier
+                .requestMatchers(HttpMethod.PUT, "/api/customer-cards/**").authenticated()
+
+                // delete everything can only manager
                 .requestMatchers(HttpMethod.DELETE,
                         "/api/categories/**",
                         "/api/products/**",
@@ -51,18 +64,38 @@ public class SecurityConfiguration {
                         "/api/employees/**",
                         "/api/customer-cards/**",
                         "/api/checks/**")
-                .hasRole("MANAGER")
+                .hasRole("ROLE_MANAGER")
 
-*/
+                // get all or get by id can both manager and cashier
+                .requestMatchers(HttpMethod.GET,
+                        "/api/categories", "/api/categories/**",
+                        "/api/products",  "/api/products/**",
+                        "/api/store-products", "/api/store-products/**",
+                        "/api/employees", "/api/employees/**",
+                        "/api/customer-cards", "/api/customer-cards/**",
+                        "/api/checks", "/api/checks/**",
+                        "/api/sales")
+                .authenticated()
 
+                // determine the total number of a certain product sold for a certain time can only manager
+                .requestMatchers(HttpMethod.GET, "/api/products-number/**").hasRole("ROLE_MANAGER")
 
-                //.anyRequest().authenticated()
+                // get store product details by UPC can both manager and cashier
+                .requestMatchers(HttpMethod.GET, "/api/store-products-details/**").authenticated()
+
+                // determine the total sum from range of checks can only manager
+                .requestMatchers(HttpMethod.GET, "/api/checks-sum").hasRole("ROLE_MANAGER")
+
+                // get employee's contact info by surname can only manager
+                .requestMatchers(HttpMethod.GET, "/api/employees/contact-info").hasRole("ROLE_MANAGER")
+ */
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 }
